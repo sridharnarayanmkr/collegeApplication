e80f8e8972edbe59152dd3e7a1c5c82f8d61cd44
sridharnarayanmkr <sridharnarayanmkr@gmail.com>
2020-01-29 22:59:33 +0530

    Removed the unnecessary codes

--- src/json/example/SampleJSON.java
+++ src/json/example/SampleJSON.java
@@ -3,10 +3,6 @@ package json.example;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -19,32 +15,7 @@ public class SampleJSON {
 		Employee e = objmap.readValue(data, Employee.class);
 		System.out.println(e);
 	}
-	public static Employee createEmployee() {
-		Employee emp = new Employee();
-		emp.setId(100);
-		emp.setName("David");
-		emp.setPermanent(false);
-		emp.setPhoneNumbers(new long[] { 123456, 987654 });
-		emp.setRole("Manager");
 
-		Address add = new Address();
-		add.setCity("Bangalore");
-		add.setStreet("BTM 1st Stage");
-		add.setZipcode(560100);
-		emp.setAddress(add);
-
-		List<String> cities = new ArrayList<String>();
-		cities.add("Los Angeles");
-		cities.add("New York");
-		emp.setCities(cities);
-
-		Map<String, String> props = new HashMap<String, String>();
-		props.put("salary", "1000 Rs");
-		props.put("age", "28 years");
-		emp.setProperties(props);
-		
-		return emp;
-	}
 
 }
 
